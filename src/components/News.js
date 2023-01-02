9350eb5e662be53a36a90193a1a5f77ccb0c633a
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-02 19:12:33 +0545

    some work on newsitem

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,22 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component.
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container' my-3>
+        <h2>NewsMania-Top HeadLines</h2>
+       <div className="row">
+        <div className="col-md-4">
+        <NewsItem title="myTitle" description="myDesc"/>
+        </div>
+        <div className="col-md-4">
+        <NewsItem title="myTitle" description="myDesc"/>
+        </div>
+        <div className="col-md-4">
+        <NewsItem title="myTitle" description="myDesc"/>
+        </div>
+       
+        
+       </div>
+       
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description} =this.props;
     return (
-      <div className='container'>
-        This is a news item.
+      <div className='container my-3'>
+        <div className="card" style={{width: "18rem"}}>
+  <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{description}</p>
+    <a href="/" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
       </div>
     )
   }
