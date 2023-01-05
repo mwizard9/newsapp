8c7b7ceb4036c7d2c171844d7ab1efa86da49431
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-05 15:38:46 +0545

    changes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div className='container my-3'>
         <div className="card" style={{ width: "18rem" }}>
-          <img src={!imageUrl?"https://img.etimg.com/thumb/msid-96674810,width-1070,height-580,imgsize-45744,overlay-economictimes/photo.jpg":imageUrl} alt="..." />
+          <img src={!imageUrl?"https://img.etimg.com/thumb/msid-96674810,width-1070,height-580,imgsize-45744,overlay-economictimes/photo.jpg":imageUrl} alt="...images loading" />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
