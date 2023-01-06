2c215100f7ce4a75d18eeb42ceacd8aecfde1342
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-06 12:03:58 +0545

    adding proptypes

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8
+  }
+
+  static propTypes = {
+    country: this.propTypes.string,
+    pageSize:propTypes.number
+  }
 
 
   constructor(){
