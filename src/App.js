f2cb670d95c263e61266346edf96b7f46ba1b2aa
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-08 13:01:04 +0545

    adding routes component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
-import{
+import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link 
-}from "react-router-dom";
+  Link
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div className='contaner'>
-        <NavBar/>
-        <News pageSize={5} country="in" category="science"/>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<News pageSize={5} country="in" category="general" />}>
+            </Route>
+            <Route exact path="/business" element={<News pageSize={5} country="in" category="general" />}>
+            </Route>
+          </Routes>
+        </Router>
       </div>
     )
   }
