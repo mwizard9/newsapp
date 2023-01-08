e72616698c2588dfae81fd33e54b0f15bcbfe9d7
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-08 12:39:10 +0545

    import switch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link 
+}from "react-router-dom";
 
 export default class App extends Component {
   render() {
