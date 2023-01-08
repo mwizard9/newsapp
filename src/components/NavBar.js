ad03dd8c40cd877b7dc9ffc35977a65b5ba2c947
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-08 13:09:56 +0545

    replace href by to

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,38 +10,38 @@ export class NavBar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <Link className="navbar-brand" href="/">NewsMania</Link>
+    <Link className="navbar-brand" to="/">NewsMania</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" href="/about">About</Link>
+          <Link className="nav-link" to="/about">About</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" href="/business">Business</Link>
+          <Link className="nav-link" to="/business">Business</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" href="/entertainment">Entertainment</Link>
+          <Link className="nav-link" to="/entertainment">Entertainment</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" href="/general">General</Link>
+          <Link className="nav-link" to="/general">General</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" href="/health">Health</Link>
+          <Link className="nav-link" to="/health">Health</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" href="/science">Science</Link>
+          <Link className="nav-link" to="/science">Science</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" href="/sport">Sport</Link>
+          <Link className="nav-link" to="/sport">Sport</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" href="/technology">Technology</Link>
+          <Link className="nav-link" to="/technology">Technology</Link>
         </li>
       
         
