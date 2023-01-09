3e96117ca540ef2acc346f70086ccc8d5b20fe88
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-09 14:47:40 +0545

    adding author and date

--- src/components/News.js
+++ src/components/News.js
@@ -72,7 +72,8 @@ export class News extends Component {
        <div className="row">
        {!this.state.loading && this.state.articles.map((element)=>{
         return <div className="col-md-4" key={element.url}>
-        <NewsItem title={element.title?element.title.slice(0,40):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+        <NewsItem title={element.title?element.title.slice(0,40):""} description={element.description?element.description.slice(0,88):""} 
+        imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishshedAt}/>
         </div>
         })} 
        </div>
