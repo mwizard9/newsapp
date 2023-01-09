ef056c1b82e7f5e02b1f690dc4a22f3354b12364
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-09 15:12:33 +0545

    adding source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl, author, date} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
       <div className='container my-3'>
         <div className="card">
           <img src={!imageUrl?"https://img.etimg.com/thumb/msid-96674810,width-1070,height-580,imgsize-45744,overlay-economictimes/photo.jpg":imageUrl} alt="...images loading" />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">{title}...  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    {source}</span></h5>
             <p className="card-text">{description}...</p>
             <p className='cart-text'><small className='text-muted'>By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
             <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
